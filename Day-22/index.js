9151a263b1959178478878070c80dcb010912207
shubhammt123 <shubhammt123@gmail.com>
2025-09-15 15:53:30 +0530

    git & import export

---
+++ Day-22/index.js
@@ -0,0 +1,19 @@
+// Import export
+
+// cjs - common js
+// mjs - module js
+
+// const {sum , sub , multi  , div} = require("./mathOperations.js");
+
+import a , {sub , sum , div} from "./mathOperations.js";
+
+// add(1000,200);
+
+// function sum(){
+//     console.log("Sum function called");
+// }
+
+sum(1000,200)
+sub(1000,200)
+a(1000,200)
+div(1000,200)
---
+++ Day-22/mathOperations.js
@@ -0,0 +1,15 @@
+export function sum(a,b){
+    console.log(a+b)
+}
+export function sub(a,b){
+    console.log(a-b)
+}
+export default function multi(a,b){
+    console.log(a*b)
+}
+export function div(a,b){
+    console.log(a/b)
+}
+
+// default export - only one element can be default export in one file
+// named export - can be multiple
