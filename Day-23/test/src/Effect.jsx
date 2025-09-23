c7c710ba2330de67bef9fd1eb8168064bec2eb2f
shubhammt123 <shubhammt123@gmail.com>
2025-09-23 13:43:44 +0530

    Use Effect

--- Day-23/test/src/Effect.jsx
+++ Day-23/test/src/Effect.jsx
@@ -1,24 +1,55 @@
 import React, { useEffect, useState } from 'react'
+import Card from './Card';
 
 const Effect = () => {
-    const [data , setData] = useState([]);
-    const fetchData = async ()=>{
-        try {
-            const response = await fetch("https://jsonplaceholder.typicode.com/users");
-            const productData = await response.json();
-            setData(productData);
-        } catch (error) {
-            console.log(error)
-        }
-    }
+  // const [counter,setCounter] = useState(0);
+  // const [number , setNumber] = useState(0);
 
-    useEffect(()=>{
-      fetchData();
-    },[]);
-    
-    console.log(data);
+  const [data , setData] = useState([]);
+
+  const fetchData = async ()=>{
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+// useEffect(()=>{
+//   console.log(counter);
+//   console.log("Hello world");
+//   fetchData();
+// },[number]);
+
+useEffect(()=>{
+  fetchData();
+},[]);
+
   return (
-    <div>Effect</div>
+    <>
+      {/* <button onClick={()=>{
+        setCounter(counter-1)
+      }}>Dec Count</button>
+      <p>Count : {counter}</p>
+      <button onClick={()=>{
+        setCounter(counter+1)
+      }}>Inc Count</button>
+      <br />
+      <button onClick={()=>{
+        setNumber(number-1)
+      }}>Dec Number</button>
+      <p>Number : {number}</p>
+      <button onClick={()=>{
+        setNumber(number+1)
+      }}>Inc Number</button> */}
+      {data.map((item)=>{
+        return (
+          <Card title={item.title} description={item.description} image={item.image} />
+        )
+      })}
+    </>
   )
 }
 
