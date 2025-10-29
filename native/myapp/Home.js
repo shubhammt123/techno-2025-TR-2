27adc8e4afe3f498f29393c1dc4823e5aa02b394
shubhammt123 <shubhammt123@gmail.com>
2025-10-29 11:09:54 +0530

    Alert

--- native/myapp/Home.js
+++ native/myapp/Home.js
@@ -1,12 +1,32 @@
-import { Button, Modal, StyleSheet, Text, View } from 'react-native'
+import { Alert, Button, Modal, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 
 const Home = () => {
     const [visible , setVisible] = useState(false);
+    const showAlert = ()=>{
+        Alert.alert(
+            "Hello",
+            "This is the demo message",
+            [
+                {
+                text : "ok" ,
+                onPress : ()=>{
+                    console.log("Ok button pressed")
+                }
+            },
+            {
+                text : "cancel",
+                onPress : ()=>{
+                    console.log("Cancel pressed")
+                }
+            }
+        ]
+        )
+    }
   return (
     <View>
-        <Button title="Open Model" onPress={()=>{setVisible(true)}} />
-      {/* <Text style={styles.para}>Home</Text> */}
+        {/* <Button title="Open Model" onPress={()=>{setVisible(true)}} />
+      <Text style={styles.para}>Home</Text>
       <Modal visible={visible} animationType='fade' transparent={true} onRequestClose={()=>{
         setVisible(false)
       }}>
@@ -16,7 +36,9 @@ const Home = () => {
             </Text>
             <Button title='Close Model' onPress={()=>{setVisible(false)}} />
         </View>
-      </Modal>
+      </Modal> */}
+      <Button title="Open Alert" onPress={showAlert} />
+
     </View>
   )
 }
